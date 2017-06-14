ea54b7429374056683effc58a3ffa203ca8e321c
TheLegitSlickCoder <bagalkote.family@outlook.com>
2017-06-14 17:39:19 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Windows.Forms;
+
+namespace Scriptr
+{
+  internal static class Program
+  {
+    [STAThread]
+    private static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run((Form) new Form1());
+    }
+  }
+}
